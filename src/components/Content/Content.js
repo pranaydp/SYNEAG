f8e637f63eb094a786139bf617aba66beae50a3a
swapnil <swapkurjekar17@gmail.com>
2020-05-25 09:10:45 +0530

    table dummy

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -15,10 +15,40 @@ class Content extends Component {
 								</div>
 								<div className='box-body'>
 									<div className='row'>
-										<div className='col-md-8'>
-											<p className='text-center'>
-												<strong></strong>
-											</p>
+										<div className='container col-md-8 col-lg-6'>
+											<div className=''>
+												<h2>Hover Rows</h2>
+												<p>
+													The .table-hover class enables a hover state on table
+													rows:
+												</p>
+												<table className='table table-hover'>
+													<thead>
+														<tr>
+															<th>Firstname</th>
+															<th>Lastname</th>
+															<th>Email</th>
+														</tr>
+													</thead>
+													<tbody>
+														<tr>
+															<td>John</td>
+															<td>Doe</td>
+															<td>john@example.com</td>
+														</tr>
+														<tr>
+															<td>Mary</td>
+															<td>Moe</td>
+															<td>mary@example.com</td>
+														</tr>
+														<tr>
+															<td>July</td>
+															<td>Dooley</td>
+															<td>july@example.com</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
 										</div>
 									</div>
 								</div>
