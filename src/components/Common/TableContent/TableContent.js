40b3a1902810cb6b384b0a7d4740f7bfa60d6e17
pranay pokharkar <pranay.pokharkar@tecinnobiz.com>
2020-06-02 18:01:35 +0530

    my projects json changes and condition added.

--- src/components/Common/TableContent/TableContent.js
+++ src/components/Common/TableContent/TableContent.js
@@ -7,6 +7,9 @@ import TableCell from "@material-ui/core/TableCell";
 import Tooltip from '@material-ui/core/Tooltip';
 import QueuePlayNext from '@material-ui/icons/QueuePlayNext';
 import ExitToApp from '@material-ui/icons/ExitToApp';
+import AddToQueue from '@material-ui/icons/AddToQueue';
+
+
 import IconButton from '@material-ui/core/IconButton';
 
 const TableContent = (props) => {
@@ -16,24 +19,35 @@ const TableContent = (props) => {
 			{props.content !== null
 				? props.content.map((row) => ( 
 						<TableRow hover key={row.name}>
-							<TableCell component='th' scope='row' align='center'>
-								{row.Projectname}
-							</TableCell>
-							<TableCell align='center'>{row.Frameworktype}</TableCell>
-							<TableCell align='center'>{row.Suntype}</TableCell>
+						{row.extensionApproved === true ?
+							<TableCell component='th' scope='row' align='center' style={{ color:"red"}}>
+								{row.projectName}
+							</TableCell> : <TableCell component='th' scope='row' align='center'>
+							{row.projectName}
+						    </TableCell> }
+							<TableCell align='center'>{row.FrameworkType}</TableCell>
+							<TableCell align='center'>{row.FrameworkSubtype}</TableCell>
 							
-							<TableCell align='center'>
+							<TableCell align='center'> 
+							{row.onBoard === true ?
+							<Tooltip title="OnBoard">
+							<IconButton aria-label="AddToQueue">
+                            <AddToQueue color="primary" />
+							</IconButton>
+							</Tooltip> : ""}
+							{row.addMaturities === true ?
 							<Tooltip title="Add Maturity">
 							<IconButton aria-label="QueuePlayNext">
                             <QueuePlayNext color="primary" />
 							</IconButton>
-							</Tooltip>
+							</Tooltip> : ""}
 							
+							{row.addQuality === true ?
 							<Tooltip title="Add Quality">
 							<IconButton aria-label="ExitToApp">
 							<ExitToApp color="primary" />
 							</IconButton>
-						    </Tooltip>
+						    </Tooltip> : ""}
 							</TableCell>
 						</TableRow>
 				  ))
--- src/components/Common/TableHeader/TableHeader.js
+++ src/components/Common/TableHeader/TableHeader.js
@@ -12,7 +12,7 @@ const TableHeader = (props) => {
 		<TableHead>
 			<TableRow>
 				{props.heading.map((text, index) => {
-					return <TableCell align='center'>{text}</TableCell>;
+					return <TableCell align='center'><b>{text}</b></TableCell>;
 				})}
 			</TableRow>
 		</TableHead>
